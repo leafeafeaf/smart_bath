d0be533f446070b3585a6ad2f3ec642b6b757f39
leafeafeaf <rlatjdals369@naver.com>
2023-04-18 18:54:23 +0900

    0418_02

--- src/components/HomeBathSetting.vue
+++ src/components/HomeBathSetting.vue
@@ -5,27 +5,33 @@
     <div id="TempSetting">
       <div style="height: 30px">
         <div class="left_box">목욕 온도</div>
-        <div class="right_box">40도</div>
+        <div class="right_box">{{ tempvalue }}도</div>
       </div>
-      <vue-slider v-model="value" />
+      <vue-slider v-model="tempvalue" :min="30" :max="45" :interval="1" />
     </div>
 
     <div id="LevelSetting">
       <div style="height: 30px">
         <div class="left_box">물 높이</div>
-        <div class="right_box">40%</div>
+        <div class="right_box">{{ levelvalue }}%</div>
       </div>
-      <vue-slider v-model="value" />
+      <vue-slider v-model="levelvalue" :interval="10" />
     </div>
   </div>
 </template>
 <script>
+import { reactive, toRefs } from "vue";
 import VueSlider from "vue-slider-component";
 import "vue-slider-component/theme/default.css";
 
 export default {
   components: {
     VueSlider,
+  },
+  setup() {
+    //슬라이더의 값(목욕 온도, 물 높이) 외부로 값을 빼야함
+    const data = reactive({ tempvalue: 40, levelvalue: 30 });
+    return toRefs(data);
   },
 };
 </script>
--- src/components/HomeCleanSetting.vue
+++ src/components/HomeCleanSetting.vue
@@ -16,52 +16,40 @@ import { defineComponent, reactive, toRefs } from "vue";
 const scrollpickerOptions = [
   [
     {
-      label: "5",
-      value: 5,
-    },
-    {
       label: "10",
       value: 10,
-    },
-    {
-      label: "15",
-      value: 15,
     },
     {
       label: "20",
       value: 20,
     },
     {
-      label: "25",
-      value: 25,
-    },
-    {
       label: "30",
       value: 30,
-    },
-    {
-      label: "35",
-      value: 35,
     },
     {
       label: "40",
       value: 40,
     },
     {
-      label: "45",
-      value: 45,
-    },
-    {
       label: "50",
       value: 50,
     },
     {
-      label: "55",
-      value: 55,
-    },
-    {
       label: "60",
       value: 60,
+    },
+    {
+      label: "70",
+      value: 70,
+    },
+    {
+      label: "80",
+      value: 80,
+    },
+    {
+      label: "90",
+      value: 90,
     },
   ],
 ];
