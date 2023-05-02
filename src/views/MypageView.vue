9d2b7b518f55eb3c7e12d62f4bb5e4e491ecdde1
leafeafeaf <rlatjdals369@naver.com>
2023-05-02 16:53:50 +0900

    0502

--- src/views/MypageView.vue
+++ src/views/MypageView.vue
@@ -1,13 +1,45 @@
+<!--내페이지 탭-->
 <template>
   <div>
-    <div class="title">
-      <h1 class="left_box">내 페이지</h1>
+    <header>
+      <h2 class="left_box">내 페이지</h2>
       <router-link to="/setting"
-        ><img src="../assets/settings.png" alt="" class="setting right_box"
+        ><font-awesome-icon icon="fa-solid fa-gear" class="right_box"
       /></router-link>
+    </header>
+
+    <div class="box">사용자 이름</div>
+
+    <div class="box">
+      <div>
+        <div><font-awesome-icon icon="fa-regular fa-clock" class="" />기록</div>
+      </div>
+
+      <div class="historyBox">
+        <font-awesome-icon icon="fa-solid fa-clock" />
+        <br />
+        평균 목욕 시간<br />
+        32분
+      </div>
+      <div class="historyBox">
+        <font-awesome-icon icon="fa-regular fa-clock" />
+        <br />
+        최대 목욕 시간<br />
+        57분
+      </div>
+      <div class="historyBox">
+        <font-awesome-icon icon="fa-solid fa-bath" />
+        <br />
+        목욕 빈도<br />
+        월 3번
+      </div>
+      <div class="historyBox">
+        <font-awesome-icon icon="fa-solid fa-temperature-half" />
+        <br />
+        평균 온도<br />
+        57분
+      </div>
     </div>
-    <div>사용자 이름</div>
-    <div>기록</div>
     <Footer />
   </div>
 </template>
@@ -21,8 +53,16 @@ export default {
 };
 </script>
 <style scoped>
-.title {
-  height: 30px;
+header {
+  height: 50px;
+}
+.historyBox {
+  border: 1px solid black;
+  border-radius: 25%;
+  text-align: center;
+  display: inline-block;
+  width: 30%;
+  margin: 3%;
 }
 .left_box {
   float: left;
